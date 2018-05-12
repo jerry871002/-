4bbfb0a4f7ed893baf1026417e1d092b0299bcf3
Jerry <jerry871002@gmail.com>
2018-05-13 04:14:16 +0800

    add namespce test example

---
+++ namespace/main.cpp
@@ -0,0 +1,9 @@
+#include "someFunc.h"
+
+using namespace Andy;
+using namespace Jerry;
+
+int main() {
+    printName(); // error: call to 'printName' is ambiguous
+    return 0;
+}
---
+++ namespace/someFunc.h
@@ -0,0 +1,13 @@
+#include <iostream>
+
+namespace Andy {
+    void printName() {
+        std::cout << "Hi Andy!" << std::endl;
+    }
+} /* Andy */
+
+namespace Jerry {
+    void printName() {
+        std::cout << "Hi Jerry!" << std::endl;
+    }
+} /* Jerry */
